a4d5e156792e9c6b12897a9177f7ae9f2faf6003
yuta0709 <58354812+yuta0709@users.noreply.github.com>
2025-02-20 21:48:30 +0900

    ルーテイングを修正

--- app/routes/_auth.login/route.tsx
+++ app/routes/_auth.login/route.tsx
@@ -21,7 +21,7 @@ export async function clientAction({ request }: Route.ClientActionArgs) {
     password: password as string,
   });
   localStorage.setItem("token", token.token);
-  return redirect("/dashboard");
+  return redirect("/");
 }
 
 export default function Login({ actionData }: Route.ComponentProps) {
